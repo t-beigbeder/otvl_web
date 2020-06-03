4661eceedcb593ac4c68e31728edbd0d1cf6b45b
thierry_otvl <thierry_otvl@otvl.org>
2020-06-03 11:53:30 +0200

    WIP: docker configurable site / TODO: fix assets in vuejs server

--- dev/shell/run_compose_up.sh
+++ dev/shell/run_compose_up.sh
@@ -14,11 +14,18 @@ else
 fi
 
 export VAR_DATA_TMP=${base_dir}/dev/tmp
+if [ -z "$1" ] ; then
+  data_origin="${base_dir}/server/docker_data"
+else
+  data_origin="$1"
+fi
 
 true && \
   echo rm -rf ${VAR_DATA_TMP}/* && \
-  cp -r ${base_dir}/server/docker_data/. ${VAR_DATA_TMP}/ && \
+  info "Exporting site data from ${data_origin} in ${VAR_DATA_TMP}" && \
+  cp -r ${data_origin}/. ${VAR_DATA_TMP}/ && \
   run_command docker-compose -f ${base_dir}/dev/compose/docker-compose.yml up -d && \
   true || exit 1
+info "By default, the demo site is available at https://vjs-dev-host:9443"
 info "Stop the stack with ${base_dir}/dev/shell/run_compose_down.sh"
 exit 0
